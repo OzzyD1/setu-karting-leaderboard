a54e16fc8b9b75b5b170149890fd896d4743cb4b
OzzyD1 <ozzydomarkas@gmail.com>
2024-03-13 17:40:51 +0000

    Changed over to use DataGrid instead of cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import * as React from "react";
 import { Cup } from "./icons/Cup";
 import leaderboard from "./leaderboard.js";
-
-const sort = (leaderboard) => {
-    return leaderboard.sort((a, b) => {
-        return a.time - b.time;
-    });
-};
+import { DataGrid } from "@mui/x-data-grid";
+import Typography from "@mui/material/Typography";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const getStudentClass = (student) => {
     if (student.time < 22) {
@@ -17,36 +18,40 @@ const getStudentClass = (student) => {
     }
 };
 
+const rows = leaderboard.map((student) => ({
+    id: student.student_id,
+    cup: getStudentClass(student),
+    name: student.name,
+    lapTime: student.time,
+}));
+
+const columns = [
+    {
+        field: "cup",
+        headerName: "Cup",
+        width: 100,
+        renderCell: (params) => <Cup fill={params.row.cup} />,
+    },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "lapTime", headerName: "Lap Time", width: 150 },
+];
+
 function App() {
-    const sortedLeaderboard = sort(leaderboard);
     return (
-        <div
-            className="App"
-            style={{
-                alignItems: "center",
-                display: "flex",
-                flexDirection: "column",
-            }}
-        >
-            <h1>Leaderboard</h1>
-            {sortedLeaderboard.map((student, index) => {
-                return (
-                    <div>
-                        {/* <Card>
-                            <CardContent>
-                                <Image floated="right" size="mini">
-                                    <Cup fill={getStudentClass(student)} />
-                                </Image>
-                                <CardHeader>{student.name}</CardHeader>
-                                <CardMeta>{student.student_id}</CardMeta>
-                                <CardDescription>
-                                    Time: {student.time}
-                                </CardDescription>
-                            </CardContent>
-                        </Card> */}
-                    </div>
-                );
-            })}
+        <div>
+            <Typography variant="h2">SETU Karting Leaderboard</Typography>
+            <div style={{ height: "100%", width: "100%" }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    sortModel={[
+                        {
+                            field: "lapTime",
+                            sort: "asc",
+                        },
+                    ]}
+                />
+            </div>
         </div>
     );
 }
