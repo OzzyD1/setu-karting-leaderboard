f40b41abc6e97aebfa60ca20f123837ede04e509
Ozzy Domarkas <94358381+OzzyD1@users.noreply.github.com>
2024-03-21 20:54:43 +0000

    Update drivers.js

--- src/drivers.js
+++ src/drivers.js
@@ -3,7 +3,7 @@ const drivers = [
     { student_id: 20093532, name: "Lorna O'Dwyer", time: 22.094 },
     { student_id: 20106584, name: "Jamie Doyle", time: 22.121 },
     { student_id: 20000003, name: "Luka Krezo", time: 22.547 },
-    { student_id: 20000004, name: "Dylan Rockett", time: 23.259 },
+    { student_id: 20106593, name: "Dylan Rockett", time: 23.259 },
     { student_id: 20000005, name: "Atila Astalos", time: 23.259 },
 ];
 
