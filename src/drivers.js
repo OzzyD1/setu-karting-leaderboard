7bf3f5b875df728d77cad911dce559d05ad9df17
OzzyD1 <ozzydomarkas@gmail.com>
2024-09-19 19:55:06 +0100

    updated drivers

--- src/drivers.js
+++ src/drivers.js
@@ -1,11 +1,20 @@
 const drivers = [
-    { student_id: 20068200, name: "Ozzy Domarkas", time: 22.446 },
-    { student_id: 20093532, name: "Lorna O'Dwyer", time: 22.094 },
-    { student_id: 20106584, name: "Jamie Doyle", time: 22.121 },
-    { student_id: 10000003, name: "Luka Krezo", time: 22.547 },
-    { student_id: 20106593, name: "Dylan Rockett", time: 23.057 },
-    { student_id: 20107376, name: "Atila Astalos", time: 22.99 },
-    { student_id: 10000004, name: "Nicholas", time: 25.072 },
+    { student_id: 1, name: "Ozzy Domarkas", time: 22.569 },
+    { student_id: 2, name: "Jamie Doyle", time: 22.399 },
+    { student_id: 3, name: "Rian Doyle", time: 22.616 },
+    { student_id: 4, name: "Peter Stanev", time: 22.794 },
+    { student_id: 5, name: "Jamie Tobin", time: 23.259 },
+    { student_id: 6, name: "Hannah O'Connor", time: 23.371 },
+    { student_id: 7, name: "Nathan McNamara", time: 24.325 },
+    { student_id: 8, name: "Justin Malins", time: 23.539 },
+    { student_id: 9, name: "Eoghan Dowling", time: 23.566 },
+    { student_id: 10, name: "Darragh Whitney", time: 23.586 },
+    { student_id: 11, name: "Dylan Shupe", time: 24.072 },
+    { student_id: 12, name: "Ronan Duggan", time: 25.846 },
+    { student_id: 13, name: "Lara Morosso", time: 26.104 },
+    { student_id: 14, name: "Sam Boyle", time: 23.426 },
+    { student_id: 15, name: "Ben Power", time: 23.416 },
+    { student_id: 16, name: "Ryan Sexton", time: 23.356 },
 ];
 
 export default drivers;
