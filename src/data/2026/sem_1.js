349de55eb53acd994e173c450a07515338771f9c
jackgear <20107021@mail.wit.ie>
2026-09-16 14:37:36 +0100

    update sem_1.js

---
+++ src/data/2026/sem_1.js
@@ -0,0 +1,188 @@
+export const drivers_sem1_2026 = [
+//  {student_id: 1, name: "Ozzy Domarkas", time: }
+//  {student_id: 2, name: "Jamie Doyle", time: }
+    {student_id: 3, name: "Rian Doyle", time: 22.108 },
+//  {student_id: 4, name: "Peter Stanev", time:  },
+//  {student_id: 5, name: "Jamie Tobin", time:  },
+//  {student_id: 6, name: "Hannah O'Connor", time: }
+//  {student_id: 7, name: "Nathan McNamara", time: }
+//  {student_id: 8, name: "Justin Malins", time:  },
+//  {student_id: 9, name: "Eoghan Dowling", time:  },
+//  {student_id: 10, name: "Darragh Whitney", time:  },
+//  {student_id: 11, name: "Dylan Shupe", time:  },
+//  {student_id: 12, name: "Ronan Duggan", time:  },
+//  {student_id: 13, name: "Laura Moroso", time:  },
+    {student_id: 14, name: "Sam Boyle", time: 22.231 },
+//  {student_id: 15, name: "Ben Power", time:  },
+//  {student_id: 16, name: "Ryan Sexton", time:  },
+//  {student_id: 17, name: "Richard Murphy", time: }
+//  {student_id: 18, name: "Ellen Donnelly", time:  },
+//  {student_id: 19, name: "Jamie Halloran", time:  },
+//  {student_id: 20, name: "Dylan Rockett", time:  },
+//  {student_id: 21, name: "Dean Horgan", time:  },
+//  {student_id: 22, name: "Niko Wisniewski", time:  },
+    {student_id: 23, name: "Jack Gear", time: 22.462 },
+//  {student_id: 24, name: "", time: },
+//  {student_id: 25, name: "Harry Watkin", time: }
+//  {student_id: 26, name: "Adam Raher", time:  },
+//  {student_id: 27, name: "Patryk Pawlak", time: }
+//  {student_id: 28, name: "Cillian O'Connor", time:  },
+//  {student_id: 29, name: "Simon Harte", time:  },
+//  {student_id: 30, name: "Leon Wallis", time:  },
+//  {student_id: 31, name: "Ryan Price", time:  },
+//  {student_id: 32, name: "Nathan Fitzgerald", time:  },
+//  {student_id: 33, name: "Josh Murphy", time:  },
+//  {student_id: 34, name: "Evan O'Neill", time: }
+//  {student_id: 35, name: "Ryan Hodge", time:  },
+//  {student_id: 36, name: "Tomas Guiney", time:  },
+//  {student_id: 37, name: "Sarah Niedermayr", time:  },
+//  {student_id: 38, name: "Lydia Crotty", time:  },
+//  {student_id: 39, name: "Buse Hoggoren", time:  },
+//  {student_id: 40, name: "Jack Downey", time:  },
+//  {student_id: 41, name: "Ciara Lennox", time:  },
+//  {student_id: 42, name: "Simone", time:  },
+//  {student_id: 43, name: "James Walker", time:  },
+//  {student_id: 44, name: "Archy Sergefjs", time:  },
+//  {student_id: 45, name: "Cillian O'Brien", time:  },
+//  {student_id: 46, name: "Noah Gibbons", time:  },
+//  {student_id: 47, name: "Liam Kenny", time:  },
+//  {student_id: 48, name: "Jack Mackey", time:  },
+//  {student_id: 49, name: "Marcus Wearen", time:  },
+//  {student_id: 50, name: "Rosanne Coe", time: }
+//  {student_id: 51, name: "Usama Khan", time:  },
+//  {student_id: 52, name: "Grzegorz Brzeczyszczykiewicz", time:  },
+//  {student_id: 53, name: "", time: },
+//  {student_id: 54, name: "Namra Fatima", time:  },
+//  {student_id: 55, name: "Rostyslav Norman", time:  },
+//  {student_id: 56, name: "Chris Bolger", time:  },
+//  {student_id: 57, name: "Haziq Khurshid", time:  },
+//  {student_id: 58, name: "Taras", time:  },
+//  {student_id: 59, name: "Maks", time:  },
+//  {student_id: 60, name: "Kieran O'Neill", time:  },
+//  {student_id: 61, name: "Xavier", time:  },
+    {student_id: 62, name: "Colm Power Pierce", time: 21.945 },
+//  {student_id: 63, name: "Jamie Malone", time:  },
+//  {student_id: 64, name: "Jessica Flynn", time:  },
+//  {student_id: 65, name: "Niamh O'Keeffe", time:  },
+//  {student_id: 66, name: "Leah O'Donoghue", time:  },
+//  {student_id: 67, name: "Shannon Bennett", time:  },
+//  {student_id: 68, name: "Ayo", time:  },
+//  {student_id: 69, name: "Daniel Sevenster", time:  },
+//  {student_id: 70, name: "Taisce Ryan", time:  },
+//  {student_id: 71, name: "Patrick Whelan", time:  },
+//  {student_id: 72, name: "Niall Doherty", time:  },
+    {student_id: 73, name: "Scott Bowdren", time: 22.207 },
+//  {student_id: 74, name: "Ryan Kelly", time:  },
+//  {student_id: 75, name: "Jack Murphy", time:  },
+//  {student_id: 76, name: "Peter Murphy", time:  },
+//  {student_id: 77, name: "Justin Whelan", time:  },
+//  {student_id: 79, name: "Megan        ", time:  }, //surname
+//  {student_id: 80, name: "Becky        ", time:  }, //surname
+//  {student_id: 81, name: "Shane Nolan", time:  },
+//  {student_id: 82, name: "Aine Whelan", time:  },
+//  {student_id: 83, name: "Jasmine O'Shea Rush", time: },
+//  {student_id: 84, name: "Daniel Greany", time:  },
+//  {student_id: 85, name: "Colin Kelly", time:  },
+//  {student_id: 86, name: "Niall O'Brien", time:  },
+//  {student_id: 87, name: "Stephan Price", time:  },
+//  {student_id: 88, name: "Ciara Burke", time:  },
+//  {student_id: 89, name: "Ava Carr", time:  },
+//  {student_id: 90, name: "Ben Cheal", time: }
+//  {student_id: 91, name: "", time: },
+//  {student_id: 92, name: "", time: },
+//  {student_id: 93, name: "Caragh Mackey", time: }
+//  {student_id: 94, name: "Stefan Price", time:  },
+//  {student_id: 95, name: "Conor Nolan", time:  },
+//  {student_id: 96, name: "Liam Walsh", time:  },
+//  {student_id: 97, name: "Eric Butler", time:  },
+//  {student_id: 98, name: "Jason Walsh", time: }
+//  {student_id: 99, name: "Hayden English", time: }
+//  {student_id: 100, name: "Alex Dunne", time: }
+//  {student_id: 101, name: "Dylan Agnew", time: }
+//  {student_id: 102, name: "Dan Fagan", time: },
+//  {student_id: 103, name: "Luke Kenny", time: }
+//  {student_id: 104, name: "Henry Thompson", time: },
+//  {student_id: 105, name: "Hazel English", time: },
+//  {student_id: 106, name: "Brenda Gallachoir", time: },
+//  {student_id: 107, name: "Kash Khan", time:  },
+//  {student_id: 108, name: "Ciara O'Riordan", time: },
+//  {student_id: 109, name: "Ruben Barrasa", time: }
+//  {student_id: 110, name: "James Martin", time: }
+//  {student_id: 111, name: "Szymon Kondratowicz", time: },
+//  {student_id: 112, name: "Anna Tupalska", time: },
+//  {student_id: 113, name: "Berkay Eren", time: },
+//  {student_id: 114, name: "Josh Maher", time: },
+//  {student_id: 115, name: "Olivier Grzywaczewski", time: },
+//  {student_id: 116, name: "Joshua Fourie", time: }
+//  {student_id: 117, name: "Umar Usman", time: },
+//  {student_id: 118, name: "Lee Flynn", time: }
+//  {student_id: 119, name: "Ronan Dove", time: },
+//  {student_id: 120, name: "Maciej Pawlak", time: },
+//  {student_id: 121, name: "Lucas Pessin", time: }
+//  {student_id: 122, name: "Clara Becq", time: }
+//  {student_id: 123, name: "Eimear Curtin", time: },
+//  {student_id: 124, name: "Mahdi Mahdi", time: },
+//  {student_id: 125, name: "Rupien O'Leary", time: },
+//  {student_id: 126, name: "Cody Doran", time: }, //cory?
+//  {student_id: 127, name: "Darragh O'Grady", time: },
+//  {student_id: 128, name: "James O'Leary", time: },
+//  {student_id: 129, name: "Matthew O'Connel", time: },
+//  {student_id: 130, name: "Mark Kennedy", time:  },
+//  {student_id: 131, name: "Conor O'Sullivan", time: },
+//  {student_id: 132, name: "Paddy Walsh", time: },
+//  {student_id: 133, name: "John Wade", time: },
+//  {student_id: 134, name: "Cody Doran", time: },
+//  {student_id: 135, name: "Tiarnan Malone", time: },
+//  {student_id: 136, name: "Dillon Huberty", time: }
+//  {student_id: 137, name: "Luke Corcoran", time: },
+//  {student_id: 138, name: "Maksim Karakulin", time: },
+//  {student_id: 139, name: "Thomas Carlier", time: },
+//  {student_id: 140, name: "Simon Wally", time: },
+//  {student_id: 141, name: "Emile Binet", time: },
+//  {student_id: 142, name: "Ashir Jehangir", time: },
+//  {student_id: 143, name: "Serine Louai", time: },
+//  {student_id: 144, name: "Denis Lamber", time: }, //not a member
+//  {student_id: 145, name: "Berkay Eren", time:  },
+//  {student_id: 146, name: "Luke McGrath", time: }
+//  {student_id: 147, name: "Dawid Sumera", time: },
+//  {student_id: 148, name: "George-Cristoph Burghelea", time:  },
+//  {student_id: 149, name: "Annabel Allenden", time: }
+//  {student_id: 150, name: "Finley Power", time:  },
+//  {student_id: 151, name: "Florian Nizon", time: },
+//  {student_id: 152, name: "Victor Idowu", time: },
+//  {student_id: 153, name: "Amandine Carlier", time: },
+//  {student_id: 154, name: "Ben O'Gorman", time: }
+//  {student_id: 155, name: "Jake Kirwan", time: }
+//  {student_id: 156, name: "Ivo", time: } //not a member?
+//  {student_id: 157, name: "Lana", time: }
+//  {student_id: 158, name: "Kieran Macaraeg", time: }
+//  {student_id: 159, name: "Ryan Murphy", time: } not a member?
+//  {student_id: 160, name: "Szymon Magulla", time: }
+//  {student_id: 161, name: "Adam O'Connor", time: }
+//  {student_id: 162, name: "A Grandemange", time: } not a member
+//  {student_id: 163, name: "Mathew Palac", time: }
+//  {student_id: 164, name: "James McDonald", time: } not a member
+//  {student_id: 165, name: "Luke Maher", time: }
+//  {student_id: 166, name: "Joseph Fennin", time: }
+//  {student_id: 167, name: "Tadgh Silke", time: } not a member
+    {student_id: 168, name: "Gustaw Tarka", time: 22.768},
+//  {student_id: 169, name: "Desislava Pencheva", time: }
+//  {student_id: 170, name: "Hadi Ramadan", time: }
+//  {student_id: 171, name: "Fiona Doyle", time: } not a member
+//  {student_id: 172, name: "Erica Atkin", time: } not a member
+//  {student_id: 173, name: "Méabh Donnellan", time: } not a member
+//  {student_id: 174, name: "Timmy Shittu", time: } not a member
+//  {student_id: 175, name: "Brayan Mani", time: } not a member
+//  {student_id: 176, name: "Donal Doherty", time: }
+//  {student_id: 177, name: "Samuel Domorad", time: } not a member
+//  {student_id: 178, name: "Patrick Walsh", time: }
+//  {student_id: 179, name: "Evan Hopkins", time: } not a member
+//  {student_id: 180, name: "Sean Kelly", time: }
+//  {student_id: 181, name: "David Murphy", time: }
+//  {student_id: 182, name: "Pahim Segundo Amador", time: }
+//  {student_id: 183, name: "Dutkiewicz Igor", time: }
+//  {student_id: 184, name: "Oliwia Spolnik", time: }
+//  {student_id: 185, name: "Shanmuga Priya Vijaya Baskar", time: }
+//  {student_id: 186, name: "Arianna Kaleniuk", time: }
+];
+export default drivers_sem1_2026;
